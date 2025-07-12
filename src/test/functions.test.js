1129d16d733f2cbc38b3e9fc6b7271d87349d1e0
Manpreet Singh <mskhurana729@gmail.com>
2025-07-12 10:01:42 -0400

    test: Write test for calculator add function

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -1,10 +1,15 @@
 import { capitalizeStr } from "../js/functions";
-import { reverseStr } from "../js/functions";
+import { reverseStr, Calculator } from "../js/functions";
+
 describe("Test Different Functions", () => {
   it("Capitalize the string", () => {
     expect(capitalizeStr("abcd")).toBe("Abcd");
   });
   it("Reverse the string", () => {
     expect(reverseStr("abcd")).toBe("dcba");
+  });
+  it("Calculator", () => {
+    let calculator = new Calculator();
+    expect(calculator.add(1, 2)).toBe(3);
   });
 });
