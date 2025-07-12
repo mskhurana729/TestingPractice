ec9f99b212319d7e3788509ab2ea7c46e38aa5cc
Manpreet Singh <mskhurana729@gmail.com>
2025-07-11 22:58:33 -0400

    test: Write test for reverseStr function

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -1,6 +1,10 @@
-import capitalizeStr from "../js/functions";
+import { capitalizeStr } from "../js/functions";
+import { reverseStr } from "../js/functions";
 describe("Test Different Functions", () => {
   it("Capitalize the string", () => {
     expect(capitalizeStr("abcd")).toBe("Abcd");
+  });
+  it("Reverse the string", () => {
+    expect(reverseStr("abcd")).toBe("dcba");
   });
 });
