643f97e99521738c421a997c4c26d8f328f7ede0
Manpreet Singh <mskhurana729@gmail.com>
2025-07-12 10:06:37 -0400

    test: Write test for divide method of class Calculator

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -12,5 +12,6 @@ describe("Test Different Functions", () => {
     let calculator = new Calculator();
     expect(calculator.add(1, 2)).toBe(3);
     expect(calculator.subtract(2, 1)).toBe(1);
+    expect(calculator.divide(4, 2)).toBe(2);
   });
 });
