2c61af7bb02aff956bc37aaa31b320346028e357
Manpreet Singh <mskhurana729@gmail.com>
2025-07-12 10:08:50 -0400

    test: Write test for multiply method of class calculator

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -13,5 +13,6 @@ describe("Test Different Functions", () => {
     expect(calculator.add(1, 2)).toBe(3);
     expect(calculator.subtract(2, 1)).toBe(1);
     expect(calculator.divide(4, 2)).toBe(2);
+    expect(calculator.multiply(1, 2)).toBe(2);
   });
 });
